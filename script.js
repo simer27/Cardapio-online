39e53c1d0b91a8a655656d0117a01d83e4a587b7
Simeao Almeida <simer27@yahoo.com>
2024-04-30 16:56:03 -0300

    funções add cartModal, atualizar cartModal, estilização do cartModal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menu = document.getElementById("menu");
 const cartBtn = document.getElementById("cart-btn");
 const cartModal = document.getElementById("cart-modal");
-const cartItemsContainer = document.getElementById("cart-Items");
+const cartItemsContainer = document.getElementById("cart-items");
 const cartTotal = document.getElementById("cart-total");
 const checkout = document.getElementById("checkout-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
@@ -9,8 +9,11 @@ const cartCounter = document.getElementById("cart-count");
 const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
+let cart = [];
+
 
 cartBtn.addEventListener("click", function() {
+  updateCartModal();
   cartModal.style.display = "flex";
 })
 
@@ -23,3 +26,64 @@ cartModal.addEventListener("click", function(event) {
     cartModal.style.display = "none";
   }
 })
+
+menu.addEventListener("click", function(event){
+  let parentButton = event.target.closest(".add-to-cart-btn")
+
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name")
+    const price = parseFloat(parentButton.getAttribute("data-price"))
+
+    addToCart(name, price);
+  }
+})
+
+function addToCart(name, price) {
+  const existingItem = cart.find(item => item.name === name)
+
+  if (existingItem) {
+    existingItem.quantity += 1;
+
+  }else{
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    })
+  }
+  updateCartModal();
+}
+
+function updateCartModal(){
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+
+  cart.forEach(item => {
+    const cartItemElement = document.createElement("div");
+    cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col");
+
+    cartItemElement.innerHTML =`
+      <div class= "flex items-center justify-between">
+      <div>
+        <p class="font-bold">${item.name}</p>
+        <p>Qtd: ${item.quantity}</p>
+        <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+      </div>
+
+        <button>
+          Remover
+        </button>
+
+    </div>
+    `
+    total += item.price * item.quantity;
+
+    cartItemsContainer.appendChild(cartItemElement)
+  })
+  cartTotal.innerHTML = total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  cartCounter.innerHTML = cart.length;
+}
