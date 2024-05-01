8954768e02be5e3e41f0dc8468cdb1c8a47d57b5
Simeao Almeida <simer27@yahoo.com>
2024-05-01 12:19:34 -0300

    Função "removeItemCart"  remove a quantidade de 1 item com a validação se o item especifico do cardápio for maior que 1 ele remove apenas 1 item, se não ele remove o item do CartModal e atualiza ele

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function updateCartModal(){
         <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
       </div>
 
-        <button>
+        <button class="remove-from-cart-btn" data-name="${item.name}">
           Remover
         </button>
 
@@ -87,3 +87,29 @@ function updateCartModal(){
 
   cartCounter.innerHTML = cart.length;
 }
+
+cartItemsContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const name = event.target.getAttribute("data-name")
+
+    removeItemCart(name);
+  }
+})
+
+
+function removeItemCart(name) {
+  const index = cart.findIndex(item => item.name === name);
+  if (index !== -1) {
+    const item = cart[index];
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      updateCartModal();
+      return;
+    }
+
+    cart.splice(index, 1);
+    updateCartModal();
+
+  }
+}
+
