c3cc0aa34e4818d7ba82c2bddb758e5ad49678c0
Simeao Almeida <simer27@yahoo.com>
2024-05-02 12:35:05 -0300

    correção da data e horario de funcinonamento

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestauranteOpen() {
   const data = new  Date();
   const hora = data.getHours();
-  return hora >= 11 && hora <= 22;
+  return hora >= 18 && hora <= 22;
 }
 
 const spanItem = document.getElementById("date-span");
